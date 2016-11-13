4426651ca901b25734e6b78cd277b17d9472074c
pipeolis <>
2016-11-13 18:43:17 -0500

    Creacion vista

--- CompiladorProgVI/src/compiladorprogvi/Formatos.java
+++ CompiladorProgVI/src/compiladorprogvi/Formatos.java
@@ -11,15 +11,5 @@ public class Formatos {
     public static String[] palabrasReservadas = {"INT","DECIMAL","CHAR","STRING","BOOLEAN","VOID","IF","FOR"};  
     public static String[] operadores = {"+","-","*","/","%","^"};
     public static String[] operadoresLogicos = {"<",">",">=","<=","=","AND","OR","NOT"};
-    public static String[] asignacion = {":="};
-    
-    private final String mundo;
-    public Formatos(String mundo) {
-        this.mundo = mundo;
-    }
-
-    public Formatos() {
-    }
-    
-    
+    public static String[] asignacion = {":="};    
 }
